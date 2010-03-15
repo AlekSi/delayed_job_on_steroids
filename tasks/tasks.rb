169263526960f5bd1de76526e0226334feca478f
Aleksey Palazhchenko <alek.silverstone@gmail.com>
2010-03-15 16:31:40 +0300

    Fix task description.

--- tasks/tasks.rb
+++ tasks/tasks.rb
@@ -8,7 +8,7 @@ namespace :jobs do
     Delayed::Job.delete_all
   end
 
-  desc "Start a delayed_job worker. Options: MIN_PRIORITY, MAX_PRIORITY, JOB_TYPES."
+  desc "Start a delayed_job worker (options: MIN_PRIORITY, MAX_PRIORITY, JOB_TYPES)."
   task :work => [:merb_env, :environment] do
     Delayed::Worker.new(:min_priority => ENV['MIN_PRIORITY'], :max_priority => ENV['MAX_PRIORITY'],
       :job_types => ENV['JOB_TYPES']).start
