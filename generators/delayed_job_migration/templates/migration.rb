2a714108dbf2374efc37209638013df1c772ef95
Aleksey Palazhchenko <alek.silverstone@gmail.com>
2010-03-16 11:47:05 +0300

    last_error is text now.

--- generators/delayed_job_migration/templates/migration.rb
+++ generators/delayed_job_migration/templates/migration.rb
@@ -5,7 +5,7 @@ class CreateDelayedJobs < ActiveRecord::Migration
       t.integer  :attempts, :default => 0      # Provides for retries, but still fail eventually.
       t.text     :handler                      # YAML-encoded string of the object that will do work
       t.string   :job_type                     # Class name of the job object, for type-specific workers
-      t.string   :last_error                   # reason for last failure (See Note below)
+      t.text     :last_error                   # reason for last failure (See Note below)
       t.datetime :run_at                       # When to run. Could be Time.now for immediately, or sometime in the future.
       t.datetime :locked_at                    # Set when a client is working on this object
       t.datetime :failed_at                    # Set when all retries have failed (actually, by default, the record is deleted instead)
