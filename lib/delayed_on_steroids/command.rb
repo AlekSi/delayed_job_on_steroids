c7eface976804d701c403421d7efae83a7ca8316
Aleksey Palazhchenko <alek.silverstone@gmail.com>
2010-03-19 18:21:17 +0300

    Make it possible to run as daemon… again.

--- lib/delayed_on_steroids/command.rb
+++ lib/delayed_on_steroids/command.rb
@@ -12,11 +12,12 @@ module Delayed
       @worker_count = 1
 
       ARGV.clone.options do |opts|
-        opts.separator ""
+        opts.separator "Options:"
+        opts.on('--worker-name=name', String, 'Worker name. Default is auto-generated.') { |n| @options[:name] = n }
         opts.on('--min-priority=number', 'Minimum priority of jobs to run.') { |n| @options[:min_priority] = n }
         opts.on('--max-priority=number', 'Maximum priority of jobs to run.') { |n| @options[:max_priority] = n }
         opts.on('--job-types=type', String, 'Type of jobs to run.') { |t| @options[:job_types] = t }
-#        opts.on("-d", "--daemon", "Make worker run as a Daemon.") { @options[:detach] = true }
+        opts.on("-d", "--daemon", "Make worker run as a Daemon.") { @options[:daemon] = true }
 #        opts.on('-n', '--number-of-workers=number', "Number of unique workers to spawn.") { |n| @worker_count = (n.to_i rescue 1) }
 #        opts.on("-e", "--environment=name", String,
 #          "Specifies the environment to run this worker under (test/development/production).",
@@ -27,28 +28,20 @@ module Delayed
       end
     end
 
-    def daemonize
-      run
-    end
+    def run
+      Process.daemon if @options[:daemon]
 
-    def run(worker_name = nil)
       Dir.chdir(RAILS_ROOT)
 
-      # Replace the default logger... too bad Rails doesn't make this easier
-      Rails.logger.instance_eval do
-        @log.reopen File.join(RAILS_ROOT, 'log', 'delayed_job.log')
-      end
-      Delayed::Worker.logger = Rails.logger
       ActiveRecord::Base.connection.reconnect!
 
-      Delayed::Job.worker_name = "#{worker_name} #{Delayed::Job.worker_name}"
+      Delayed::Worker.name = @options[:name]
 
-      Delayed::Worker.new(@options).start
+      Delayed::Worker.instance.start
     rescue => e
-      logger.fatal e
-      STDERR.puts e.message
+      logger.fatal(e)
+      STDERR.puts(e.message)
       exit 1
     end
-
   end
 end
