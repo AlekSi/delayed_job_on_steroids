84c9776bc0a5cf1a16d8ed3bbc8b87b832fa5fd3
Aleksey Palazhchenko <alek.silverstone@gmail.com>
2010-03-19 19:10:50 +0300

    Daemon works.

--- lib/delayed_on_steroids/command.rb
+++ lib/delayed_on_steroids/command.rb
@@ -4,21 +4,22 @@ module Delayed
 
   # Used by script/delayed_job: parses options, sets logger, invokes Worker.
   class Command
-    attr_accessor :worker_count
 
     def initialize
-      @options = {:quiet => false}
-
       @worker_count = 1
+      @run_as_daemon = false
 
       ARGV.clone.options do |opts|
         opts.separator "Options:"
-        opts.on('--worker-name=name', String, 'Worker name. Default is auto-generated.') { |n| @options[:name] = n }
-        opts.on('--min-priority=number', 'Minimum priority of jobs to run.') { |n| @options[:min_priority] = n }
-        opts.on('--max-priority=number', 'Maximum priority of jobs to run.') { |n| @options[:max_priority] = n }
-        opts.on('--job-types=type', String, 'Type of jobs to run.') { |t| @options[:job_types] = t }
-        opts.on("-d", "--daemon", "Make worker run as a Daemon.") { @options[:daemon] = true }
-#        opts.on('-n', '--number-of-workers=number', "Number of unique workers to spawn.") { |n| @worker_count = (n.to_i rescue 1) }
+        opts.on('--worker-name=name', String, 'Worker name. Default is auto-generated.') { |n| Delayed::Worker.name = n }
+        opts.on('--min-priority=number', Integer, 'Minimum priority of jobs to run.')    { |n| Delayed::Worker.min_priority = n }
+        opts.on('--max-priority=number', Integer, 'Maximum priority of jobs to run.')    { |n| Delayed::Worker.max_priority = n }
+        opts.on('--job-types=types', String, 'Type of jobs to run.')                     { |t| Delayed::Worker.job_types = t.split(',') }
+        opts.on("-d", "--daemon", "Make worker run as a Daemon.")                        { @run_as_daemon = true }
+        opts.on('-n', '--number-of-workers=number', Integer, "Number of unique workers to spawn. Implies -d option if number > 1.") do |n|
+          @worker_count = ([n, 1].max rescue 1)
+          @run_as_daemon ||= (@worker_count > 1)
+        end
 #        opts.on("-e", "--environment=name", String,
 #          "Specifies the environment to run this worker under (test/development/production).",
 #          "Default: development") { |e| @options[:environment] = e }
@@ -28,18 +29,25 @@ module Delayed
       end
     end
 
+    def spawn_workers
+      return if @worker_count == 1
+      while @worker_count > 0
+        it_is_parent = fork
+        return unless it_is_parent
+        @worker_count -= 1
+      end
+      exit 0
+    end
+
     def run
-      Process.daemon if @options[:daemon]
+      spawn_workers
+      Process.daemon if @run_as_daemon
 
       Dir.chdir(RAILS_ROOT)
 
-      ActiveRecord::Base.connection.reconnect!
-
-      Delayed::Worker.name = @options[:name]
-
       Delayed::Worker.instance.start
     rescue => e
-      logger.fatal(e)
+      logger.fatal(e) if defined?(logger)
       STDERR.puts(e.message)
       exit 1
     end
